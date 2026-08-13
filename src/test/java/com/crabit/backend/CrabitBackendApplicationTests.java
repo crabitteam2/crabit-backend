fc3d9a1eb29fc748d9f81861bc69945beb6452fe
jjoonleo <jjoonleo@gmail.com>
2026-08-13 15:37:27 +0900

    chore: initialize Spring Boot backend

---
+++ src/test/java/com/crabit/backend/CrabitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crabit.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrabitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
